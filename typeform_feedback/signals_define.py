aafcc9a3857a21f0c24491e693a98a98f9933e4f
Javier Sújar <javier.sujar@gmail.com>
2019-10-14 14:44:19 +0200

    Minor changes

--- typeform_feedback/signals_define.py
+++ typeform_feedback/signals_define.py
@@ -1,5 +1,5 @@
 from django.dispatch import Signal
 
 
-new_user_typeform_response = Signal(providing_args=['uuid'])
+new_user_typeform_response = Signal(providing_args=['uuid', 'response'])
 user_response_approved = Signal(providing_args=['uuid'])
